91e199b041933b4d404812e34e76fcd3da661194
lins <123linslouis@gmail>
2024-03-13 14:23:49 +0530

    updated code

--- android/app/src/main/java/com/example/lins/platformview/platformview_r_n_d/view/AndroidVideoPlayer.java
+++ android/app/src/main/java/com/example/lins/platformview/platformview_r_n_d/view/AndroidVideoPlayer.java
@@ -3,84 +3,49 @@ package com.example.lins.platformview.platformview_r_n_d.view;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.io.InputStream;
-
 import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.platform.PlatformView;
 
-public class AndroidVideoPlayer implements PlatformView {
+import java.io.IOException;
+import java.io.InputStream;
 
-    private final SurfaceView surfaceView;
-    private Bitmap imageBitmap = null;
+public class AndroidVideoPlayer implements PlatformView, MethodChannel.MethodCallHandler {
+
+    private final ImageView imageView;
 
     public AndroidVideoPlayer(Context context, BinaryMessenger messenger, int viewId) {
-        surfaceView = new SurfaceView(context);
+        imageView = new ImageView(context);
+        MethodChannel methodChannel = new MethodChannel(messenger, "lins.platform.learn/VideoPlayer_" + viewId);
+        methodChannel.setMethodCallHandler(this);
 
-
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(@NonNull SurfaceHolder holder) {
-                // Load the image from assets on a separate thread
-                new Thread(() -> {
-                    try {
-                        InputStream inputStream = context.getAssets().open("cat.jpg");
-                        imageBitmap = BitmapFactory.decodeStream(inputStream);
-                        inputStream.close();
-                        draw();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-            }
-
-            @Override
-            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-                draw();
-            }
-
-            @Override
-            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-                // Handle surface destruction if necessary
-            }
-        });
+        try {
+            // Load the image from assets
+            InputStream inputStream = context.getAssets().open("cat.jpg");
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageView.setImageBitmap(bitmap);
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void draw() {
-        if (imageBitmap != null) {
-            Canvas canvas = surfaceView.getHolder().lockCanvas();
-            if (canvas != null) {
-                // Center the image in the SurfaceView
-                int canvasWidth = canvas.getWidth();
-                int canvasHeight = canvas.getHeight();
-                int imageWidth = imageBitmap.getWidth();
-                int imageHeight = imageBitmap.getHeight();
-                float scale = Math.min((float) canvasWidth / imageWidth, (float) canvasHeight / imageHeight);
-
-                float x = (canvasWidth - imageWidth * scale) / 2;
-                float y = (canvasHeight - imageHeight * scale) / 2;
-
-                canvas.save();
-                canvas.scale(scale, scale);
-                canvas.drawBitmap(imageBitmap, x / scale, y / scale, null);
-                canvas.restore();
-
-                surfaceView.getHolder().unlockCanvasAndPost(canvas);
-            }
-        }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        // Implement any method calls if necessary
+        result.notImplemented();
     }
 
     @Nullable
     @Override
     public View getView() {
-        return surfaceView;
+        return imageView;
     }
 
     @Override
