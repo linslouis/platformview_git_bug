5b65266ced9c372ee02bf7de2dbf934a6086bc4d
lins <123linslouis@gmail>
2024-03-19 17:06:24 +0530

    version surface view

--- android/app/src/main/java/com/example/lins/platformview/platformview_r_n_d/view/AndroidSurfaceView.java
+++ android/app/src/main/java/com/example/lins/platformview/platformview_r_n_d/view/AndroidSurfaceView.java
@@ -3,82 +3,73 @@ package com.example.lins.platformview.platformview_r_n_d.view;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.util.TypedValue;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ScrollView;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import java.io.IOException;
 
+import com.example.lins.platformview.platformview_r_n_d.R;
+
+import java.io.IOException;
 import io.flutter.Log;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.platform.PlatformView;
 
 public class AndroidSurfaceView implements PlatformView, SurfaceHolder.Callback {
-    private final ScrollView scrollView;
-    private final FrameLayout frameLayout;
-    private final SurfaceView surfaceView;
+    private static final String TAG = "VideoSurfaceViewPluginTest";
+    private final Context context;
+    private LinearLayout rootView;
+    private SurfaceView surfaceView;
     private final MediaPlayer mediaPlayer;
 
-    private static final String TAG = "VideoSurfaceViewPluginTest";
-
     public AndroidSurfaceView(Context context, BinaryMessenger viewId, Object args) {
-        scrollView = new ScrollView(context);
-        scrollView.setLayoutParams(new ScrollView.LayoutParams(
-                ScrollView.LayoutParams.MATCH_PARENT,
-                ScrollView.LayoutParams.MATCH_PARENT));
-
-        frameLayout = new FrameLayout(context);
-        scrollView.addView(frameLayout, new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.MATCH_PARENT));
-
-        surfaceView = new SurfaceView(context);
-        // Set fixed dimensions for SurfaceView (300x250)
-        int fixedWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, context.getResources().getDisplayMetrics());
-        int fixedHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 250, context.getResources().getDisplayMetrics());
-        FrameLayout.LayoutParams surfaceParams = new FrameLayout.LayoutParams(fixedWidth, fixedHeight, Gravity.CENTER);
-        frameLayout.addView(surfaceView, surfaceParams);
-
-        surfaceView.getHolder().addCallback(this);
-
+        this.context = context;
         mediaPlayer = new MediaPlayer();
+        initializeMediaPlayer();
+        initializeView();
+    }
+
+    private void initializeMediaPlayer() {
         try {
             mediaPlayer.setDataSource(context, Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
             mediaPlayer.prepareAsync();
-            mediaPlayer.setOnPreparedListener(mp -> mp.start());
+            mediaPlayer.setOnPreparedListener(MediaPlayer::start);
             mediaPlayer.setLooping(true);
         } catch (IOException e) {
-            Log.e(TAG, "Error setting data source for mediaPlayer", e);
+            e.printStackTrace();
         }
     }
 
-    @Override
-    public void surfaceCreated(@NonNull SurfaceHolder holder) {
-        mediaPlayer.setDisplay(holder);
-    }
-
-    @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-        Log.d(TAG, "Surface changed: format=" + format + ", width=" + width + ", height=" + height);
-    }
-
-    @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-        if (mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-        }
-        mediaPlayer.release();
+    private void initializeView() {
+        rootView = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.layout, null);
+        surfaceView = rootView.findViewById( R.id.surfaceView);
+        surfaceView.getHolder().addCallback(this);
     }
 
     @Nullable
     @Override
     public View getView() {
-        return scrollView; // Return the ScrollView that wraps the SurfaceView
+        return rootView;
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        mediaPlayer.setDisplay(holder);
+        Log.d(TAG, "Surface created");
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+        Log.d(TAG, "Surface changed to format: " + format + " width: " + width + " height: " + height);
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+        Log.d(TAG, "Surface destroyed");
     }
 
     @Override
@@ -89,4 +80,3 @@ public class AndroidSurfaceView implements PlatformView, SurfaceHolder.Callback 
         mediaPlayer.release();
     }
 }
-
